bcb3427e4046de42e754c437c4235f2015a01075
Mohammed Abo abdo <m.aboabdo@yahoo.com>
2020-06-06 18:24:45 +0200

    improevment frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import{BrowserRouter as Router,Route} from 'react-router-dom';
+
 import './App.css';
 import NavBar from './components/layouts/Navbar';
 import Footer from './components/layouts/Footer';
 import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/> 
-      <Landing />
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/> 
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
